a37d62bb7061306552b6ccb8d5c20448a6b9346d
johnny-hui <johnny_hui@live.com>
2023-03-11 02:50:32 -0600

    Program is now able to parse /proc directory and all of its processes

---
+++ constants.py
@@ -0,0 +1,12 @@
+# I/O CONSTANTS
+PROC_DIRECTORY = "/proc"
+CMD_LINE = "cmdline"
+COMM = "comm"
+STATUS = "status"
+FD = "fd"
+PID = "Pid:"
+PPID = "PPid"
+
+# ERROR CONSTANTS
+USER_NOT_ROOT_ERROR = "[+] ERROR: Only the 'root' user can run this script!\n" \
+                      "[+] Please run this script again using sudo command."
--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import constants
+from constants import *
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
+def _check_if_root_user():
+    if not os.geteuid() == 0:
+        sys.exit(USER_NOT_ROOT_ERROR)
+
+
+def _parse_proc():
+    # Access all process and their contents
+    for process in os.listdir(PROC_DIRECTORY):
+        if process.isdigit():
+            proc_dir = os.path.join(PROC_DIRECTORY, process)  # Concatenate string with /proc/<process>...
+
+            # Accessing process contents
+            for item in os.listdir(proc_dir):
+                _get_selected_proc_info(item, proc_dir)
+
+            # Comment out later
+            print(f"cmdline: {cmdline}")
+            print(f"comm: {comm}")
+            print(f"pid: {pid}")
+            print(f"ppid: {ppid}")
+            print(fd)
+
+            return
+
+
+def _get_selected_proc_info(item: str, proc_directory: str):
+    global cmdline, comm, fd, ppid, pid
+    path = os.path.join(proc_directory, item)
+
+    match item:
+        case constants.CMD_LINE:
+            file = open(path, "r")
+            cmdline = file.readline().strip()
+        case constants.COMM:
+            file = open(path, "r")
+            comm = file.readline().strip()
+        case constants.FD:
+            stream = os.popen(f'sudo -S ls {path} -l')  # EXECUTE CMD: sudo ls /proc/1/fd/ -l
+            output = stream.readlines()[1:]
+            for sym_link in output:
+                fd.append(" ".join(sym_link.strip().split(" ")[8:]))
+        case constants.STATUS:
+            file = open(path, "r")
+            for line in file:
+                if line.replace("	", "").strip()[:4] == PID:
+                    pid = line.replace("	", "").strip().split(":")[1]
+                if line.replace("	", "").strip()[:4] == PPID:
+                    ppid = line.replace("	", "").strip().split(":")[1]
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cmdline, comm, fd, ppid, pid = "", "", [], "", ""
+    _parse_proc()
